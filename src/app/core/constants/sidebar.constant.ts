6a99aa85e70fcd3f5244921b7481af3313e77c9b
prashil-lonakar <prashil.l@volgainfotech.com>
2024-07-10 16:45:24 +0530

    Post and category tabs in sidebar added

---
+++ src/app/core/constants/sidebar.constant.ts
@@ -0,0 +1,41 @@
+export class SidebarConstants {
+  public static menusForAdmin: Array<{
+    id: number;
+    text: string;
+    route: string;
+    tagId: string;
+  }> = [
+    {
+      id: 1,
+      text: 'Post',
+      route: '/admin/post',
+      tagId: 'PostMenuBtn',
+    },
+    {
+      id: 2,
+      text: 'Category',
+      route: '/admin/category',
+      tagId: 'CategoryMenuBtn',
+    },
+  ];
+
+  public static menusForClient: Array<{
+    id: number;
+    text: string;
+    route: string;
+    tagId: string;
+  }> = [
+    {
+      id: 1,
+      text: 'Post',
+      route: '/post',
+      tagId: 'PostMenuBtn',
+    },
+    {
+      id: 2,
+      text: 'Category',
+      route: '/category',
+      tagId: 'CategoryMenuBtn',
+    },
+  ];
+}
--- src/app/post-login/common/sidebar/sidebar.component.ts
+++ src/app/post-login/common/sidebar/sidebar.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { SidebarConstants } from '../../../core/constants/sidebar.constant';
 @Component({
   selector: 'sidebar',
   standalone: true,
@@ -15,6 +16,7 @@ import { MatIconModule } from '@angular/material/icon';
     RouterOutlet,
     MatToolbarModule,
     MatIconModule,
+    RouterLink,
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
@@ -22,4 +24,10 @@ import { MatIconModule } from '@angular/material/icon';
 export class SidebarComponent {
   events: string[] = [];
   opened: boolean = true;
+  sideBarMenu: any[] = [];
+
+  constructor() {
+    this.sideBarMenu = SidebarConstants.menusForAdmin;
+    console.log('this.sideBarMenu', this.sideBarMenu);
+  }
 }
