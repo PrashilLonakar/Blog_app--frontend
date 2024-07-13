17b4bcf50d5d7ac7cb0fbf6df53674edf6678498
prashil-lonakar <prashil.l@volgainfotech.com>
2024-07-14 04:53:09 +0530

    Client structure updated

--- src/app/post-login/post-login.routes.ts
+++ src/app/post-login/post-login.routes.ts
@@ -9,7 +9,7 @@ export const POST_ROUTES: Routes = [
     component: PostLoginComponent,
     children: [
       {
-        path: '',
+        path: 'client',
         loadChildren: () =>
           import('./client/client.routes').then((m) => m.CLIENT_ROUTES),
       },
